948b188d5eba6e73a26051c1df4023ee9702a919
Omar <90169655+OmarHachicho@users.noreply.github.com>
2021-10-26 10:58:44 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <time.h>
+#include <cstdio>
+
+
+const a = 500
+
+const m = 500
+
+const d = 500
+
+const s = 500
+
+int main()
+{
+	system("title Calculator")
+	system("color 0b")
+	printf("Calculator Made by Omar Hachicho\nMade to test my skills.");
+	printf("This has Addition Subtraction Multiplication + Subtraction all 500");
+	printf("This works is it adds multiplies and divides and subtracts all stuff with 500");
+	//add();
+	//divide();
+	//subtract();
+	//multiply();
+	// Thats how to work each function
+}
+
+void add()
+{
+	printf(a + 100);
+	system("pause");
+}
+
+void multiply()
+{
+	printf(m * 100);
+	system("pause");
+}
+
+void subtract()
+{
+	printf(s - 100);
+	system("pause");
+}
+
+void divide()
+{
+	printf(d / 100);
+	system("pause");
+}
